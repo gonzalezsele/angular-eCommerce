852d39b85f1f975aa3847b505c00fdaa7cb25417
Selene González Blanco <sgonzalez2803@gmail.com>
2023-08-24 19:19:09 +0200

    avances & array releases

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,4 +7,66 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  releases: any []
+
+  constructor () {
+  this.releases = [
+  { 
+  "id": "1", 
+  "title": "Court of Serpents and Secrets", 
+  "author": "Eliza Raine", 
+  "publishedDate": "September 1, 2023", 
+  "image": "./assets/images/releases/cover-006.png",
+  "price": "17.47",
+  "reduced_price": "18.99"
+  },
+  { 
+  "id": "2", 
+  "title": "Assistant to the Villain", 
+  "author": "Nicole Maehrer", 
+  "publishedDate": "September 5, 2023", 
+  "image": "./assets/images/releases/cover-005.png",
+  "price": "8.27",
+  "reduced_price": "8.99"
+  },
+  { 
+  "id": "3", 
+  "title": "Fall of Ruin and Wrath", 
+  "author": "Jennifer L. Armentrout", 
+  "publishedDate": "September 12, 2023", 
+  "image": "./assets/images/releases/cover-004.png",
+  "price": "17.99",
+  "reduced_price": "20.00"
+  },
+  { 
+  "id": "4", 
+  "title": "Vows & Ruins", 
+  "author": "Helen Scheuerer", 
+  "publishedDate": "September 21, 2023", 
+  "image": "./assets/images/releases/cover-003.png",
+  "price": "13.99",
+  "reduced_price": "18.99"
+  },
+  { 
+  "id": "5", 
+  "title": "The Chalice of the Gods", 
+  "author": "Rick Riordan", 
+  "publishedDate": "September 26, 2023", 
+  "image": "./assets/images/releases/cover-002.png",
+  "price": "11.99",
+  "reduced_price": "16.99"
+  },
+  { 
+  "id": "6", 
+  "title": "The Fragile Threads of Power", 
+  "author": "V.E. Schwab", 
+  "publishedDate": "September 26, 2023", 
+  "image": "./assets/images/releases/cover-001.png",
+  "price": "17.47",
+  "reduced_price": "18.99"
+  }
+
+  ]
+  }
+
 }
